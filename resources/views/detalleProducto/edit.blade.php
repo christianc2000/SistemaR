5ef3abdad231f550ef3b5aee54357b986efd88a4
Daniel <danielmaldonadogutierrez031@gmail.com>
2021-08-17 18:31:04 -0400

    Ultima modificacion

--- resources/views/detalleProducto/edit.blade.php
+++ resources/views/detalleProducto/edit.blade.php
@@ -5,14 +5,14 @@
 @section('content_header')
 
 @section('content')
-<form action="{{route('detalleProductos.store')}}" method="POST">
+<form action="{{route('detalleProductos.update',$detalle)}}" method="POST">
     @csrf
-
+    @method('PUT')
     <br/>
 <div class="mb-3">
     <label for="" class="col-form-label">Producto contenedor</label>
     {{-- <input id="codigo" name="codigo" type="number" class="form-control" tabindex="2" required autofocus autocomplete="codigo"> --}}
-    <select class="form-select" aria-label="Default select example" id="productoContenedor" name="productoContenedor" >
+    <select class="form-select" aria-label="Default select example" id="productoContenedor" name="productoContenedor" disabled=true >
         @foreach ($producto as $prod)
         @if ($detalle->producto_A_id!=$prod->id)
             <option value="{{$prod->id}}">{{$prod->nombre}}</option>
@@ -31,7 +31,7 @@
 <div class="mb-3">
     <label for="" class="col-form-label">Producto contenido</label>
     {{-- <input id="codigo" name="codigo" type="number" class="form-control" tabindex="2" required autofocus autocomplete="codigo"> --}}
-    <select class="form-select" aria-label="Default select example" id="productoContenido" name="productoContenido" >
+    <select class="form-select" aria-label="Default select example" id="productoContenido" name="productoContenido"  disabled=true>
         @foreach ($producto as $prod)
         @if ($detalle->producto_B_id!=$prod->id)
             <option value="{{$prod->id}}">{{$prod->nombre}}</option>
